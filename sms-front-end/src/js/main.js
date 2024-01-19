8014d94561c2390b829836a14ba5dc20cdd5b0fc
Kaveendra Randeera <khkrandeera@gmail.com>
2024-01-19 16:24:56 +0530

    Add ajax put request to the front end

--- sms-front-end/src/js/main.js
+++ sms-front-end/src/js/main.js
@@ -21,5 +21,31 @@ function saveStudent() {
             alert('Error !');
         }
     });
-
 }
+
+function updateStudent() {
+    let id = $("#id-input").val();
+    let name = $("#name-input").val();
+    let address = $("#address-input").val();
+    let mobile = $("#mobile-input").val();
+
+    $.ajax({
+        method : "PUT",
+        contentType:"application/json",
+        async : true,
+        url : "http://localhost:8080/api/v1/students",
+        data : JSON.stringify({
+            "studentId": id,
+            "studentName": name,
+            "studentAddress": address,
+            "mobileNumber": mobile
+        }),
+        success : function (data){
+            alert("updated");
+        },
+        error : function (xhr, exception){
+            alert('Error !');
+        }
+    });
+}
+
